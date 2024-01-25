9feb6a2a8a66e97e664b7179c49cc84e258f8257
mugane-wahome <145966011+mugane-wahome@users.noreply.github.com>
2024-01-25 08:51:00 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
 import { useState, useEffect } from 'react';
+import './App.css';
 const Fetch = () => {
   const [photos, setPhotos] = useState([]);
+
   useEffect(() => {
-    
     fetch('https://jsonplaceholder.typicode.com/photos')
       .then((res) => {
         return res.json();
       })
       .then((photos) => {
-        console.log(photos);
         setPhotos(photos);
       });
   }, []);
-  return   photos.splice
-(0,20).map((photo) => (
-<div>
-        <img src={photo.url} alt={photo.title} width={100} />
-<p>{photo.title}</p>
-</div>
-      ))}
-    
+
+  return (
+    <div>
+      {photos.splice(0, 20).map((photo, index) => {
+        if (index % 5 === 0) {
+          return (
+            <div key={index} className="row"> 
+              {photos.slice(index, index + 5).map((photo) => (
+                <div key={photo.id} className="col-md-2"> 
+                  <img src={photo.url} alt={photo.title} width={100} />
+                  <p>{photo.title}</p>
+                </div>
+              ))}
+            </div>
+          );
+        } else {
+          return null; // 
+        }
+      })}
+    </div>
+  );
+};
 
 export default Fetch;
